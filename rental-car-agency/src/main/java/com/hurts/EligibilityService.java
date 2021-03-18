d2d637aae68c2058b0dd9ef5dce86411802986d3
dsiegal-heal <david.siegal@heal.com>
2021-03-18 11:20:31 -0700

    Added dependencies for lombok & okhttp
    Replaced getters/constructors with lombok annotations.
    Leaving it as an exercise to the user to make use of Builder Pattern
    Replaced commented out/pretend REST request call with real code

--- rental-car-agency/src/main/java/com/hurts/EligibilityService.java
+++ rental-car-agency/src/main/java/com/hurts/EligibilityService.java
@@ -1,29 +1,50 @@
 package com.hurts;
 
+import lombok.SneakyThrows;
+import okhttp3.*;
+
+import java.io.IOException;
 import java.util.logging.Logger;
 
 public class EligibilityService {
 
   boolean isTest;
   Logger logger = Logger.getLogger("EligibilityService");
+  OkHttpClient httpClient = new OkHttpClient.Builder().build();
 
   public boolean checkDoNotRentStatus(String ln) throws EligibilityServiceException {
     boolean canRent = true;
     if (isTest) {
       if (ln.endsWith("9")) { // just a made up condition for testing purposes
-        logger.severe("License has expired. Will not rent.");
-        throw new EligibilityServiceException("License has expired.");
+        logger.severe("License has expired. Do not rent.");
+        throw new EligibilityServiceException("EXPIRED");
       }
     } else {
-      // Note: this block is commented out to keep the exercise simple & build time quick. You can assume it's working code:
-
-      // String url = "https://api.hurts.com/license-service/validate/" + ln;
-      // Response response = HttpRestClient.get(url);
-      // if (response.getOutcome() == Outcome.DENIED)) {
-      //   throw new EligibilityServiceException(response.message());
-      // }
+      String outcome = callLicenseService(ln);
+      if (outcome.equals("APPROVED")) {
+        return true;
+      } else {
+        throw new EligibilityServiceException(outcome);
+      }
     }
     return canRent;
+  }
+
+
+  /**
+   * NOTE: For the purpose of this exercise, you can assume this method works correctly and that the network
+   * call will always connect & retrieve data successfully.
+   *
+   * @return a value like APPROVED, INVALID, EXPIRED, SUSPENDED, etc.
+   */
+  @SneakyThrows(IOException.class)
+  private String callLicenseService(String licenseNumber) {
+    Request request = new Request.Builder()
+        .url("https://api.hurts.com/license-service/validate/" + licenseNumber)
+        .build();
+    Call call = httpClient.newCall(request);
+    Response response = call.execute();
+    return response.body().string();
   }
 
 }
--- rental-car-agency/src/main/java/com/hurts/PriceRequest.java
+++ rental-car-agency/src/main/java/com/hurts/PriceRequest.java
@@ -3,7 +3,11 @@ package com.hurts;
 import java.util.Date;
 import java.util.List;
 import java.util.TimeZone;
+import lombok.*;
 
+@Getter
+@Builder
+@AllArgsConstructor
 public class PriceRequest {
   private String customerId;
   private Date start;
@@ -16,58 +20,4 @@ public class PriceRequest {
   public List names;
   private TimeZone timeZone;
 
-  public PriceRequest(String customerId, Date start, Date end, int length, String tier, boolean optionalInsurance,
-                      List driversLicenseNumbers, List driversBirthDates, List names, TimeZone timeZone)
-  {
-    this.customerId = customerId;
-    this.start = start;
-    this.end = end;
-    this.length = length;
-    this.tier = tier;
-    this.optionalInsurance = optionalInsurance;
-    this.driversLicenseNumbers = driversLicenseNumbers;
-    this.driversBirthDates = driversBirthDates;
-    this.names = names;
-    this.timeZone = timeZone;
-  }
-
-  public String getCustomerId() {
-    return customerId;
-  }
-
-  public Date getStart() {
-    return start;
-  }
-
-  public int getLength() {
-    return length;
-  }
-
-  public Date getEnd() {
-    return end;
-  }
-
-  public String getTier() {
-    return tier;
-  }
-
-  public boolean isOptionalInsurance() {
-    return optionalInsurance;
-  }
-
-  public List getDriversLicenseNumbers() {
-    return driversLicenseNumbers;
-  }
-
-  public List getDriversBirthDates() {
-    return driversBirthDates;
-  }
-
-  public List getNames() {
-    return names;
-  }
-
-  public TimeZone getTimeZone() {
-    return timeZone;
-  }
 }
